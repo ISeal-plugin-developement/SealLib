3fa886b34e3368045113f9359a1175af0602158c
ISeal <69968405+iseal56@users.noreply.github.com>
2024-11-13 21:48:09 +0100

    Fix SoundHelper, add entity func

--- src/main/java/dev/iseal/sealLib/Utils/SoundHelper.java
+++ src/main/java/dev/iseal/sealLib/Utils/SoundHelper.java
@@ -8,24 +8,29 @@ public class SoundHelper {
 
     public static void playSoundForPlayer(String namespace, String sound, Player plr, float pitch) {
         // Play sound
-        plr.playSound(plr, namespace + "." + sound, 1, pitch);
+        plr.playSound(plr, namespace + ":" + sound, 1, pitch);
     }
 
     public static void playSoundAroundPlayer(String namespace, String sound, Player plr, float pitch) {
         // Play sound
-        plr.getWorld().playSound(plr.getLocation(), namespace + "." + sound, 3, pitch);
+        plr.getWorld().playSound(plr.getLocation(), namespace + ":" + sound, 3, pitch);
     }
 
     public static void playSoundForAllPlayers(String namespace, String sound, float pitch) {
         // Play sound
         for (Player plr : Bukkit.getOnlinePlayers()) {
-            plr.playSound(plr, namespace + "." + sound, 1, pitch);
+            plr.playSound(plr, namespace + ":" + sound, 1, pitch);
         }
     }
 
     public static void playSoundInWorld(String namespace, String sound, float pitch, Location loc) {
         // Play sound
-        loc.getWorld().playSound(loc, namespace + "." + sound, 10^20, pitch);
+        loc.getWorld().playSound(loc, namespace + ":" + sound, 10^20, pitch);
+    }
+
+    public static void playSoundAroundEntity(String namespace, String sound, float pitch, Location loc) {
+        // Play sound
+        loc.getWorld().playSound(loc, namespace + ":" + sound, 3, pitch);
     }
 
 }
