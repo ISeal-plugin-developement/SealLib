5aa1cdf32547e762a0d81c4cb032f4ebe299f239
ISeal <69968405+iseal56@users.noreply.github.com>
2024-11-13 19:55:40 +0100

    Add SoundHelper

---
+++ src/main/java/dev/iseal/sealLib/Utils/SoundHelper.java
@@ -0,0 +1,31 @@
+package dev.iseal.sealLib.Utils;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+
+public class SoundHelper {
+
+    public static void playSoundForPlayer(String namespace, String sound, Player plr, float pitch) {
+        // Play sound
+        plr.playSound(plr, namespace + "." + sound, 1, pitch);
+    }
+
+    public static void playSoundAroundPlayer(String namespace, String sound, Player plr, float pitch) {
+        // Play sound
+        plr.getWorld().playSound(plr.getLocation(), namespace + "." + sound, 3, pitch);
+    }
+
+    public static void playSoundForAllPlayers(String namespace, String sound, float pitch) {
+        // Play sound
+        for (Player plr : Bukkit.getOnlinePlayers()) {
+            plr.playSound(plr, namespace + "." + sound, 1, pitch);
+        }
+    }
+
+    public static void playSoundInWorld(String namespace, String sound, float pitch, Location loc) {
+        // Play sound
+        loc.getWorld().playSound(loc, namespace + "." + sound, 10^20, pitch);
+    }
+
+}
